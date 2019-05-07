4e8bba0b58a4bde686d3d22ebe2c27ee9587c597
Sridhar Deivanandam (UST <U35340@ust-global.com>
2019-05-07 21:37:01 +0530

    started base class

---
+++ src/org/class1/Baseclass.java
@@ -0,0 +1,12 @@
+package org.class1;
+
+public class Baseclass {
+
+	public static void main(String[] args) {
+		//program to print the project class timing
+		String s1="project class timing is @ 9A.M";
+		System.out.println("My timing and subject for the course "+s1);
+
+	}
+
+}
